6142f3c0bb115dc6cfb06e0ae57f5ca15f6913db
Student <tlpelsmaeker@gmail.com>
2017-01-27 15:46:05 +0100

    Cleaned till validation

--- run_trec_eval.sh
+++ run_trec_eval.sh
@@ -1,2 +1,3 @@
-trec_eval -m all_trec -q $1 $2 | grep -E  "(^ndcg_cut_10\s*all)|(^P_5\s*all)|(^recall_1000\s*all)|(^map_cut_1000\s*all)"
+#!/bin/bash
+trec_eval -m all_trec -q $1 $2 | grep -E  "(^ndcg_cut_10\s)|(^P_5\s)|(^recall_1000\s)|(^map_cut_1000\s)"
 
